2985cb6e883da46f9984ab0140eea37e912694a4
yeahyashake <149016606+yahyabigceo@users.noreply.github.com>
2025-01-09 23:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (8.80%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        unique_nums = list(set(nums))
+        if len(unique_nums) < 3:
+            return max(unique_nums)
+        
+        unique_nums.sort(reverse=True)
+        return unique_nums[2]
