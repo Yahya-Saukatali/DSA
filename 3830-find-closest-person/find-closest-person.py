d0bcb4751f18a2f6faedd4286be05e97148edc9c
Yahya Saukatali <149016606+Yahya-Saukatali@users.noreply.github.com>
2025-04-20 22:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.27%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        if abs(x-z) < abs(y-z):
+            return 1
+        elif abs(x-z) > abs(y-z):
+            return 2
+        else:
+            return 0
