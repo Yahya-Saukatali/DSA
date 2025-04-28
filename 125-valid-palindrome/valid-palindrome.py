94a42401f3a2cc405ff6b1252e1445bcf560cffe
Yahya Saukatali <149016606+Yahya-Saukatali@users.noreply.github.com>
2025-04-28 14:24:44 +0530

    Time: 11 ms (35.81%) | Memory: 23.3 MB (6.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        s = ''.join(char.lower() for char in s if char.isalnum())
-        return s == s[::-1]
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=' '.join(char.lower() for char in s if char.isalnum())
+        l=0
+        r=len(s)-1
+        while l <= r:
+            if s[l]==s[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+        return True
